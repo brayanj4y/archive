681ccbd4d29946451e1f9f009b5b47d5ace3d28c
souop silvain brayan <souopsylvain@gmail.com>
2026-02-21 15:42:09 +0100

    feat(app): initialize project with Expo Router
    - Setup Expo config and entry points
    - Integrate Expo Router for navigation
    - Add Convex for backend state management
    - Configure NativeWind and Tailwind

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { Stack } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '../global.css';
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+});
+
+export default function RootLayout() {
+    return (
+        <ConvexProvider client={convex}>
+            <SafeAreaProvider>
+                <Stack>
+                    <Stack.Screen name="index" options={{ headerShown: false }} />
+                </Stack>
+            </SafeAreaProvider>
+        </ConvexProvider>
+    );
+}
